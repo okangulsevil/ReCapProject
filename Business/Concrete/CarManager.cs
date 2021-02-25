fa9285207216ae4819c7eae838c67f7acae5486b
Okan Gülsevil <okangulsevil@gmail.com>
2021-02-26 01:33:02 +0300

    3 Şubat Ödev 1 yapıldı.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -10,16 +10,52 @@ namespace Business.Concrete
     public class CarManager : ICarService
     {
         ICarDal _carDal;
-
         public CarManager(ICarDal carDal)
         {
             _carDal = carDal;
         }
 
-        // Listeleme
+        public void Add(Car car)
+        {
+            if (car.Description.Length < 2 || car.DailyPrice <= 0)
+            {
+                Console.WriteLine("Araç açıklaması 2 karakterden az olamaz !");
+                Console.WriteLine("Araç kiralama bedeli 0 dan büyük olmalı !");
+            }
+            else
+            {
+                _carDal.Add(car);
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _carDal.GetAll(c => c.Id == id);
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
         }
     }
 }
