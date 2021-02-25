9c8a6c152257ae10fa68cd43919236fd60919b4d
Okan Gülsevil <okangulsevil@gmail.com>
2021-02-25 21:54:01 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{ Id = 1, BrandId = 1, ColorId = 1, ModelYear = 2014, DailyPrice = 100, Description = "Seat"},
+                new Car{ Id = 2, BrandId = 2, ColorId = 2, ModelYear = 2017, DailyPrice = 150, Description = "Mercedes"},
+                new Car{ Id = 3, BrandId = 3, ColorId = 3, ModelYear = 2019, DailyPrice = 500, Description = "Volvo"},
+                new Car{ Id = 4, BrandId = 2, ColorId = 4, ModelYear = 2021, DailyPrice = 800, Description = "Opel"},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            return _cars.Where(c => c.Id == Id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(p => p.Id == car.Id);
+
+            carToUpdate.Id = car.Id;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
