82f1697291c007e367ec3828c0c61cd8994bc679
Okan Gülsevil <okangulsevil@gmail.com>
2021-02-26 23:55:09 +0300

    6 Şubat 2.Mola

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,13 +10,28 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            // CarTest();
+            BrandTest();
+
+        }
+
+        private static void BrandTest()
+        {
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            foreach (var brand in brandManager.GetById(2))
+            {
+                Console.WriteLine(brand.BrandName);
+            }
+        }
+
+        private static void CarTest()
+        {
             CarManager carManager = new CarManager(new EfCarDal());
 
             foreach (var car in carManager.GetCarsByBrandId(2))
             {
                 Console.WriteLine(car.Description);
             }
-
         }
     }
 }
